cb1dddf17c59aa6558539882ca1de27ae11cc3ea
Jordan Hart <p22jhart@fairfieldprep.org>
2021-11-15 07:18:36 -0800

    HW

---
+++ hw_6-3-2.py
@@ -0,0 +1,8 @@
+#question 2
+
+my_list=['1','2','3','4','5']
+
+my_list.sort 
+
+print(my_list)
+
---
+++ hw_6-3.py
@@ -0,0 +1,13 @@
+# JH 11/15/21
+
+#Question 1
+
+
+My_Ana2='Arc'
+My_Ana3='Car'
+
+print(My_Ana2 is My_Ana3)
+
+
+
+
